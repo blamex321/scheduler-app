4f3150286df6e25f0b4fd9e020b4ebf9e3b93545
blamex321 <laxman2207@outlook.com>
2023-08-24 13:49:32 +0530

    added dummy data

--- src/data.js
+++ src/data.js
@@ -5,30 +5,30 @@ export const data = [
       endDate: new Date('2021-03-29T07:14:45.000Z'),
       description: "Ensure that garbage bins are cleared from under all office desks. Garbage liners (bags) replaced where required. Approximate bins to be checked are 45'",
     }, {
-      text: 'Book Flights to San Fran for Sales Trip',
-      startDate: new Date('2021-03-29T19:00:00.000Z'),
-      endDate: new Date('2021-03-29T20:00:00.000Z'),
-      allDay: true,
-    }, {
-      text: 'Install New Router in Dev Room',
+      text: 'Staff Attendance and Briefing',
       startDate: new Date('2021-03-29T21:30:00.000Z'),
       endDate: new Date('2021-03-29T22:30:00.000Z'),
+      description : "Confirm Attendance of Staff and Brief them about the days activities. Emphasise on any points important for the particular day",
     }, {
-      text: 'Approve Personal Computer Upgrade Plan',
+      text: 'Office Table Dusting',
       startDate: new Date('2021-03-30T17:00:00.000Z'),
       endDate: new Date('2021-03-30T18:00:00.000Z'),
+      description:"Ensure all tables are dusted and cleaned. Check for dirt spots on the table which need attention. Clean the softboard lining areas and under the table. Approximate tables to be dusted are 45",
     }, {
-      text: 'Final Budget Review',
+      text: 'Washroom Cleaning - Mopping',
       startDate: new Date('2021-03-30T19:00:00.000Z'),
       endDate: new Date('2021-03-30T20:35:00.000Z'),
+      description:"Ensure wet mopping of the floor. Use Taski R1 (20 ml / 1 Ltr). Use a clean wet mop. Ensure ther eis no stink emanating after mopping",
     }, {
-      text: 'New Brochures',
+      text: 'Washroom Cleaning - WC',
       startDate: new Date('2021-03-30T21:30:00.000Z'),
       endDate: new Date('2021-03-30T22:45:00.000Z'),
+      description:"Squirt Taski R6 on the bowl, allow contact time toilet of 1 - 3 minutes depending upon the condition of bowl. Use gloves. Use a toilet brush to clean the bowl.",
     }, {
-      text: 'Install New Database',
+      text: 'Pantry Table Top Cleaning',
       startDate: new Date('2021-03-31T16:45:00.000Z'),
       endDate: new Date('2021-03-31T18:15:00.000Z'),
+      description:"Use the Furniture duster to wipe the table top clean. Ensure the legs of the table and underside fo the table are also cleaned.",
     }, {
       text: 'Approve New Online Marketing Strategy',
       startDate: new Date('2021-03-31T19:00:00.000Z'),
@@ -37,35 +37,6 @@ export const data = [
       text: 'Upgrade Personal Computers',
       startDate: new Date('2021-03-31T22:15:00.000Z'),
       endDate: new Date('2021-03-31T23:30:00.000Z'),
-    }, {
-      text: 'Customer Workshop',
-      startDate: new Date('2021-04-01T18:00:00.000Z'),
-      endDate: new Date('2021-04-01T19:00:00.000Z'),
-      allDay: true,
-    }, {
-      text: 'Prepare 2021 Marketing Plan',
-      startDate: new Date('2021-04-01T18:00:00.000Z'),
-      endDate: new Date('2021-04-01T20:30:00.000Z'),
-    }, {
-      text: 'Brochure Design Review',
-      startDate: new Date('2021-04-01T21:00:00.000Z'),
-      endDate: new Date('2021-04-01T22:30:00.000Z'),
-    }, {
-      text: 'Create Icons for Website',
-      startDate: new Date('2021-04-02T17:00:00.000Z'),
-      endDate: new Date('2021-04-02T18:30:00.000Z'),
-    }, {
-      text: 'Upgrade Server Hardware',
-      startDate: new Date('2021-04-02T21:30:00.000Z'),
-      endDate: new Date('2021-04-02T23:00:00.000Z'),
-    }, {
-      text: 'Submit New Website Design',
-      startDate: new Date('2021-04-02T23:30:00.000Z'),
-      endDate: new Date('2021-04-03T01:00:00.000Z'),
-    }, {
-      text: 'Launch New Website',
-      startDate: new Date('2021-04-02T19:20:00.000Z'),
-      endDate: new Date('2021-04-02T21:00:00.000Z'),
     },
   ];
   
