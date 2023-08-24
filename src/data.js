d8d720ee823c831a5a5e8f0a452810cab92d61f0
blamex321 <laxman2207@outlook.com>
2023-08-24 14:02:47 +0530

    disabled state

--- src/data.js
+++ src/data.js
@@ -4,31 +4,37 @@ export const data = [
       startDate: new Date('2021-03-29T14:30:00.000Z'),
       endDate: new Date('2021-03-29T07:14:45.000Z'),
       description: "Ensure that garbage bins are cleared from under all office desks. Garbage liners (bags) replaced where required. Approximate bins to be checked are 45'",
+      disabled: true,
     }, {
       text: 'Staff Attendance and Briefing',
       startDate: new Date('2021-03-29T21:30:00.000Z'),
       endDate: new Date('2021-03-29T22:30:00.000Z'),
       description : "Confirm Attendance of Staff and Brief them about the days activities. Emphasise on any points important for the particular day",
+      disabled: true,
     }, {
       text: 'Office Table Dusting',
       startDate: new Date('2021-03-30T17:00:00.000Z'),
       endDate: new Date('2021-03-30T18:00:00.000Z'),
       description:"Ensure all tables are dusted and cleaned. Check for dirt spots on the table which need attention. Clean the softboard lining areas and under the table. Approximate tables to be dusted are 45",
+      disabled: true,
     }, {
       text: 'Washroom Cleaning - Mopping',
       startDate: new Date('2021-03-30T19:00:00.000Z'),
       endDate: new Date('2021-03-30T20:35:00.000Z'),
       description:"Ensure wet mopping of the floor. Use Taski R1 (20 ml / 1 Ltr). Use a clean wet mop. Ensure ther eis no stink emanating after mopping",
+      disabled: true,
     }, {
       text: 'Washroom Cleaning - WC',
       startDate: new Date('2021-03-30T21:30:00.000Z'),
       endDate: new Date('2021-03-30T22:45:00.000Z'),
       description:"Squirt Taski R6 on the bowl, allow contact time toilet of 1 - 3 minutes depending upon the condition of bowl. Use gloves. Use a toilet brush to clean the bowl.",
+      disabled: true,
     }, {
       text: 'Pantry Table Top Cleaning',
       startDate: new Date('2021-03-31T16:45:00.000Z'),
       endDate: new Date('2021-03-31T18:15:00.000Z'),
       description:"Use the Furniture duster to wipe the table top clean. Ensure the legs of the table and underside fo the table are also cleaned.",
+      disabled: true,
     }, {
       text: 'Approve New Online Marketing Strategy',
       startDate: new Date('2021-03-31T19:00:00.000Z'),
