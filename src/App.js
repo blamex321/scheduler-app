9596c862476b49e044a123d33a91e434b055606f
blamex321 <laxman2207@outlook.com>
2023-08-24 13:00:37 +0530

    scheduler-demo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import 'devextreme/dist/css/dx.common.css';
+import './themes/generated/theme.base.css';
+import './themes/generated/theme.additional.css';
+import React from 'react';
+import { HashRouter as Router } from 'react-router-dom';
+import './dx-styles.scss';
+import LoadPanel from 'devextreme-react/load-panel';
+import { NavigationProvider } from './contexts/navigation';
+import { AuthProvider, useAuth } from './contexts/auth';
+import { useScreenSizeClass } from './utils/media-query';
+import Scheduler from 'devextreme-react/scheduler';
+
+import { data } from './data.js';
+
+const currentDate = new Date(2021, 2, 28);
+const views = ['week', 'month'];
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return(
+  <Scheduler
+    timeZone="America/Los_Angeles"
+    dataSource={data}
+    views={views}
+    defaultCurrentView="week"
+    defaultCurrentDate={currentDate}
+    height={600}
+    startDayHour={6} />);
 }
 
-export default App;
+export default function Root() {
+  const screenSizeClass = useScreenSizeClass();
+
+  return (
+    <Router>
+      <AuthProvider>
+        <NavigationProvider>
+          <div className={`app ${screenSizeClass}`}>
+            <App />
+          </div>
+        </NavigationProvider>
+      </AuthProvider>
+    </Router>
+  );
+}
